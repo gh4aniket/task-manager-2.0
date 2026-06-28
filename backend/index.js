7ce66837db5b61d8e0e6d6ce777b9e313da91d95
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2026-06-28 22:29:57 +0530

    Change CORS origin to allow all origins

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const app = express()
 // Middleware to handle cors
 app.use(
   cors({
-    origin: process.env.FRONT_END_URL || "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
